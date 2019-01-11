376b9509c439106e138008e1b3800fec3e4998e5
cmorto02 <chrismorton.cm@gmail.com>
2019-01-11 15:25:13 -0800

    well, i tried to do this with a loop first because i didnt read the instructions.

--- app.js
+++ app.js
@@ -82,12 +82,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var threearraysum = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+  var message = sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + threearraysum + ' is their sum.';
+  console.log(threearraysum, message);
+  return[threearraysum, message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -138,3 +141,19 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+
+
+
+
+
+
+
+
+// function sumArray(sumArr) { //eslint-disable-line
+//   var total = 0;
+//   for(var i = 0; i < sumArr.length; i++){
+//     total += sumArr[i];
+//     console.log(i + ' , ' + total);
+//   }
+//   var message = i + ' was passed in as an array of numbers and ' + total + ' is their sum';
+//   return[total, message];
